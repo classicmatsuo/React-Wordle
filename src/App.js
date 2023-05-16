481b4597c5bcf131f099c42d94c4bbb120b86da8
Kevin Matsuo <kmatsuo@ARDs-MacBook-Pro-2.local>
2023-05-15 21:30:28 -0500

    updated api endpoint urls

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import { useEffect, useState } from 'react'
-import Wordle from './components/Wordle'
+import { useEffect, useState } from "react";
+import Wordle from "./components/Wordle";
 
 function App() {
-  const [solution, setSolution] = useState(null)
-  
+  const [solution, setSolution] = useState(null);
+
   useEffect(() => {
-    fetch('http://localhost:3001/solutions')
-      .then(res => res.json())
-      .then(json => {
+    fetch("https://valiant-painted-tabletop.glitch.me/solutions")
+      .then((res) => res.json())
+      .then((json) => {
         // random int between 0 & 14
-        const randomSolution = json[Math.floor(Math.random()*json.length)]
-        setSolution(randomSolution.word)
-      })
-  }, [setSolution])
+        const randomSolution = json[Math.floor(Math.random() * json.length)];
+        setSolution(randomSolution.word);
+      });
+  }, [setSolution]);
 
   return (
     <div className="App">
       <h1>Wordle (Lingo)</h1>
       {solution && <Wordle solution={solution} />}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
 
 /* 
 
