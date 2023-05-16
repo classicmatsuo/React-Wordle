6ad11ad814e983f70df57ae808ec8dae8b55f14a
Kevin Matsuo <kmatsuo@ARDs-MacBook-Pro-2.local>
2023-05-16 16:02:27 -0500

    updated the modal text

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,22 +1,22 @@
-import React from 'react'
+import React from "react";
 
 export default function Modal({ isCorrect, solution, turn }) {
   return (
     <div className="modal">
       {isCorrect && (
         <div>
-          <h1>You Win!</h1>
+          <h1>Dude, are you like a genius or somethin?!</h1>
           <p className="solution">{solution}</p>
           <p>You found the solution in {turn} guesses :)</p>
         </div>
       )}
       {!isCorrect && (
         <div>
-          <h1>Nevermind</h1>
+          <h1>Dude, cmon..</h1>
           <p className="solution">{solution}</p>
-          <p>Better luck next time :)</p>
+          <p>What's wrong with you? :)</p>
         </div>
       )}
     </div>
-  )
+  );
 }
